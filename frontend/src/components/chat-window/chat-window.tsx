88c83b086254565bd80cd2b055113a2277c8b723
Dan Sze <danssze@gmail.com>
2023-09-24 19:55:59 -0400

    ADd tailwind stlying

--- frontend/src/components/chat-window/chat-window.tsx
+++ frontend/src/components/chat-window/chat-window.tsx
@@ -7,21 +7,21 @@ const ChatWindow: React.FC = () => {
   const [newMessage, setNewMessage] = useState<string>('');
   const socketRef = useRef<Socket>();
 
-  useEffect(() => {
-    socketRef.current = io('http://localhost:8000/ws', {
-      query: {
-        token: 'your-token',
-        userId: 'user-id'
-      }
-    });
-    socketRef.current.on('receive-message', (message: string) => {
-      setMessages((prev) => [...prev, message]);
-    });
+  // useEffect(() => {
+  //   socketRef.current = io('http://localhost:8000/ws', {
+  //     query: {
+  //       token: 'your-token',
+  //       userId: 'user-id'
+  //     }
+  //   });
+  //   socketRef.current.on('receive-message', (message: string) => {
+  //     setMessages((prev) => [...prev, message]);
+  //   });
 
-    return () => {
-      socketRef.current?.disconnect();
-    };
-  }, []);
+  //   return () => {
+  //     socketRef.current?.disconnect();
+  //   };
+  // }, []);
 
   const sendMessage = () => {
     if (newMessage.trim()) {
@@ -31,22 +31,24 @@ const ChatWindow: React.FC = () => {
   };
 
   return (
-    <div className="chat-window">
-      <div className="messages-container">
-        {messages.map((message, index) => (
-          <div key={index} className="message">
-            {message}
+    <div>
+      <div className="chat-window">
+        <div className="messages-container">
+          {messages.map((message, index) => (
+            <div key={index} className="message">
+              {message}
+            </div>
+          ))}
+        </div>
+        <div className="form-control w-full">
+          <label className="label">
+            <span className="label-text">Send a message</span>
+          </label>
+          <div className="join">
+            <input type="text" placeholder="I want a budget" className="input input-bordered w-full join-item" />
+            <button className="btn join-item" onClick={sendMessage}>Send</button>
           </div>
-        ))}
-      </div>
-      <div className="input-container">
-        <input
-          type="text"
-          value={newMessage}
-          onChange={(e) => setNewMessage(e.target.value)}
-          placeholder="Type a message"
-        />
-        <button onClick={sendMessage}>Send</button>
+        </div>
       </div>
     </div>
   );
